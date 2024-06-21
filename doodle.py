ebc9dc570379ff4e4fb3547aa6ce5ab4ac840a46
Ethan Jahn <ethan@lemur.local>
2024-06-21 11:44:19 -0700

    another small refactor for doodle.py

--- doodle.py
+++ doodle.py
@@ -1,5 +1,3 @@
-#dumb_walk.py
-
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -14,33 +12,22 @@ os.makedirs(save_dir, exist_ok=True)
 CMAPS = ['viridis','inferno','binary','spring','summer','autumn','winter','ocean',
 	'gist_earth','terrain','gnuplot','gnuplot2','cubehelix','gist_rainbow','twilight']
 
-# how to choose proportion of shapes:
-# 
-#	  decider
-# 0      v                                 1
-# |		      |                |           |
-# |~squiggles~|~~~~~lines~~~~~~|~~markers~~|
-# |           |                |           |
-#             S                L
+S = 0.05 # fraction of squiggles = S = 0.05
+L = 0.1  # fraction of lines   = L-S = 0.05
+#          fraction of markers = 1-L = 0.9
 
-S = 0.05 # threshold for squiggles
-L = 0.1 # threshold for lines
-
-auto_window = True    # randomly pull window size from normal distribution
-window = 0
-
-num_doodles = 100 # how many lines to draw
+AUTO_WINDOW = True # randomly pull window size from normal distribution
+NUM_DOODLES = 100 # how many doodles to draw
 
 # read image files to get next file name
-def get_next_filename(directory, base_name="doodle_"):
+def get_next_filename(directory=save_dir, base_name="doodle_"):
 	pattern = re.compile(rf"{re.escape(base_name)}(\d+)")
 	files = os.listdir(directory)
 	numbers = [int(pattern.match(f).group(1)) for f in files if pattern.match(f)]
 	next_number = max(numbers) + 1 if numbers else 1
 	return os.path.join(directory, f"{base_name}{next_number}.png")
 
-def draw_squiggle(ax,window,color):
-	# print('drawing squiggle')
+def draw_squiggle(ax,color):
 	# set up parameters
 	max_steps   = 2000   # max number of steps for a line
 	step_length = 0.005   # how long should each step be
@@ -53,15 +40,14 @@ def draw_squiggle(ax,window,color):
 	# drift = 0.995 # theoretically should unravel line if less than 1
 	drift = 1
 
-	# intialize coordinates
-	start_point = np.random.random(size=2)
-	cur_point = start_point
-	path = start_point
-	count = 0
-
 	# determine window size
-	if auto_window : window = np.random.normal(loc=0.3,scale=0.2)
-	else :			 window = window
+	if AUTO_WINDOW : window = np.random.normal(loc=0.3,scale=0.2)
+	else :			 window = 0.2
+
+	# intialize coordinates
+	cur_point = np.random.random(size=2)
+	path = cur_point
+	count = 0
 
 	# draw new points within the image boundary or below max steps
 	while (cur_point[0] < 1) and (cur_point[1] < 1) and (count < max_steps):
@@ -71,9 +57,11 @@ def draw_squiggle(ax,window,color):
 		# new_window = window
 		new_window = np.random.normal(loc=window,scale=0.001) #if scale is too high it just makes tight squiggles
 
+		# nudge angle within window
 		theta = theta + np.random.random() * new_window * np.pi * sign
 		window = new_window*drift
 
+		# step forward along path
 		cur_point = cur_point + np.array([step_length*np.cos(theta), step_length*np.sin(theta)])
 		path = np.vstack((path,cur_point))
 		count += 1
@@ -83,19 +71,13 @@ def draw_squiggle(ax,window,color):
 	ax.plot(path[:,0],path[:,1],'-',lw=lw,color=color)
 
 def draw_line(ax,color):
-	# print('drawing line')
-	start_point = np.random.random(size=2)
-	end_point =  np.random.random(size=2)
-	path = np.vstack((start_point,end_point))
-
 	# randomly pick line width
 	lw = np.random.choice([1,1,1.1,1.2,1.5,2,4])#,8,16])
-	ax.plot(path[:,0],path[:,1],'-',lw=lw,color=color)
+	ax.plot(np.random.random(size=2),np.random.random(size=2),'-',lw=lw,color=color)
 
 def draw_marker(ax,color):
-
 	# randomly decide to fill or not fill marker
-	if (np.random.random() < 1.0) : mfc = color 
+	if (np.random.random() < 0.7) : mfc = color 
 	else : 							mfc = 'none'
 
 	# choose marker style and rotate
@@ -130,7 +112,6 @@ def draw_blur(ax,cmap):
 	z += np.random.random((res,res)) / np.random.randint(1,10)
 	ax.pcolormesh(x, y, z, cmap=cmap, shading='auto',alpha=np.random.random())
 
-
 if __name__ == '__main__':
 	# set up plotting surface
 	plt.style.use('dark_background')
@@ -142,30 +123,27 @@ if __name__ == '__main__':
 	# determine colorscheme for doodles
 	colormap = plt.get_cmap(np.random.choice(CMAPS))
 
-
-	if not(auto_window):
-		window = float(input('enter multiple of pi for angle decision: '))
-
-	for n in np.arange(num_doodles):
-		this_color = colormap(n/num_doodles,1) # other colormaps: viridis, cubehelix
-		# shape = np.random.choice(['line', 'squiggle', 'marker'])
-		
+	# draw doodles
+	for n in np.arange(NUM_DOODLES):
+		this_color = colormap(n/NUM_DOODLES,1) # other colormaps: viridis, cubehelix
 		decider = np.random.random()
-		if   (decider <= squiggle_val) : draw_squiggle(ax,window,this_color)
-		elif (decider > line_val) : 	 draw_marker(ax,this_color)
-		else:							 draw_line(ax,this_color) 
+		if   (decider <= S) : draw_squiggle(ax,this_color)
+		elif (decider > L)  : draw_marker(ax,this_color)
+		else : 				  draw_line(ax,this_color) 
 
+	# set bounds and display
 	ax.set_xlim(0,1)
 	ax.set_ylim(0,1)
 	ax.set_axis_off()
 	plt.tight_layout()
 	plt.show()
 
+	# prompt to save or not save the doodle
 	yn = input('save figure, y/[n]? ')
 	if yn=='y':
-		filename = get_next_filename(save_dir)
-		print('saving figure: '+filename)
-		fig.savefig(filename,format='png',dpi=300,bbox_inches='tight')
+		# filename = get_next_filename()
+		print('saving figure: ' + get_next_filename())
+		fig.savefig(get_next_filename(),format='png',dpi=300,bbox_inches='tight')
 	else:
 		print('figure not saved')
 
